45cc4ed7283ca4d977fab166348b12f438d00a7b
EHVeil <elibchv@gmail.com>
2020-04-09 12:38:22 -0700

    header component build

---
+++ src/Title.js
@@ -0,0 +1,19 @@
+// src/Title.js
+
+import React from 'react'
+import './Title.css';
+
+function Title() {
+  return (
+    <div className='Title'>
+      <h1>
+      SFPOPOS
+        <div className='Title-Subtitle'>
+         <h4>San Francisco Privately Owned Public Open Spaces</h4>
+        </div>
+      </h1>
+    </div>
+  )
+}
+
+export default Title
