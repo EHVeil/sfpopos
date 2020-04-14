598f2b879ef2c0533cf14070f4d65e646ee4cfb7
EHVeil <elibchv@gmail.com>
2020-04-13 19:25:29 -0700

    dynamic content and footer git add .

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import Title from './Title';
 import POPOSlist from './POPOSlist.js';
+import Footer from './Footer.js';
 
 function App() {
   return (
     <div className="App">
      <Title />
      <POPOSlist />
+     <Footer />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
--- src/POPOSlist.js
+++ src/POPOSlist.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import POPOSSpace from './POPOSSpace.js';
+import './POPOSlist.css';
 
 function POPOSlist() {
   return(
